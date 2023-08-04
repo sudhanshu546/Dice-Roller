6edb686c1d96bc3edd54239015194b59a9162c58
sudhanshu546 <115442172+sudhanshu546@users.noreply.github.com>
2023-08-04 08:51:44 +0530

    Add files via upload

---
+++ MainActivity.kt
@@ -0,0 +1,36 @@
+package com.saini.diceroller
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
+import android.widget.Toast
+import java.util.*
+
+class MainActivity : AppCompatActivity() {
+    lateinit var diceImage: ImageView
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val rollButton : Button =findViewById(R.id.rollButton)
+        rollButton.text="Let's Roll"
+        rollButton.setOnClickListener {
+            Toast.makeText(this, "button clicked", Toast.LENGTH_SHORT).show()
+           rollDice()
+        }
+        diceImage=findViewById(R.id.diceImage)
+    }
+    private fun rollDice() {
+        val randomInt=Random().nextInt(6) + 1
+        val drawableResource=when (randomInt){
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+        diceImage.setImageResource(drawableResource)
+    }
+}
